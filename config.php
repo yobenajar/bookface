ab009dbd2514defdc60e8f617570b0d88ea5fa12
Yobe Najar <yoben@stud.ntnu.no>
2018-03-15 16:44:36 +0100

    First commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$dbhost = "10.10.0.248";
+$dbport = "3306";
+$db = "bookface";
+$dbuser = "bookfaceuser";
+$dbpassw = 'yytech';
+$webhost = '10.212.136.90';
+$weburl = 'http://' . $webhost ;
+?>
